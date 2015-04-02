d217aa35cca5b58d6b693a72aab3980e7166e1fa
pedroraminhas <pedro.raminhas@gmail.com>
2015-04-02 16:08:50 +0100

    Classe IMapper feita
    classe que implementa a interface IMapper feita. Função Map implementa
    um word count

---
+++ Projecto/Client/IMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PADIMapNoReduce
+{
+   public interface IMapper{
+       IList<KeyValuePair<string, string>> Map(string fileLine);
+    }
+}
